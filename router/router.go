7a219a41da6e6848f0f1b1bd1bbf70e3570ca283
ni9ht <657389517@qq.com>
2021-02-19 12:31:22 +0800

    refactor: 调整路由文件中部分中间件注册方式

--- router/router.go
+++ router/router.go
@@ -6,11 +6,19 @@ import (
 	"github.com/gogf/gf/net/ghttp"
 )
 
+var (
+	// RouteGroupPrefix 路由统一前缀
+	RouteGroupPrefix = "/v1"
+)
+
 func init() {
 	s := g.Server()
-	s.Group("/v1", func(group *ghttp.RouterGroup) {
+	// 跨域中间件
+	s.Use(middleware.CORS)
+	// 异常记录中间件
+	s.Use(middleware.ErrorLog)
+	s.Group(RouteGroupPrefix, func(group *ghttp.RouterGroup) {
 		group.Middleware(
-			middleware.CORS,
 			middleware.GetPath)
 		group.POST("/user/login", middleware.JwtMiddleware.LoginHandler)
 		group.GET("/user/logout", middleware.JwtMiddleware.LogoutHandler)
