a4fbfcc1102572bb8b17b4c5f9b5e329576bd8c3
ni9ht <657389517@qq.com>
2021-04-04 16:23:01 +0800

    feat: 增加树形结构

---
+++ library/common/tree.go
@@ -0,0 +1,68 @@
+package common
+
+// ITreeNode 要使用树形结构，对应结构体需要实现以下方法
+type ITreeNode interface {
+	GetPrimKey() int       // 主键
+	GetParentPrimKey() int // 父主键
+	GetName() string       // 节点名称
+	GetData() interface{}  // 数据
+	Root() bool            // 是否根节点
+}
+
+// TreeNode 树形结构的节点数据
+type TreeNode struct {
+	PrimKey       int         `json:"primKey"`
+	ParentPrimKey int         `json:"parentPrimKey"`
+	Name          string      `json:"name"`
+	Data          interface{} `json:"data"`
+	Children      []TreeNode  `json:"children"`
+}
+
+// GenerateTree 生成树形结构
+func GenerateTree(nodes []ITreeNode) (trees []TreeNode) {
+	if len(nodes) <= 0 {
+		return
+	}
+	var roots, childs []ITreeNode
+
+	// 分类根节点和子节点
+	for _, v := range nodes {
+		if v.Root() {
+			roots = append(roots, v)
+		} else {
+			childs = append(childs, v)
+		}
+	}
+
+	for _, root := range roots {
+		treeNode := &TreeNode{
+			Name:          root.GetName(),
+			PrimKey:       root.GetPrimKey(),
+			ParentPrimKey: root.GetParentPrimKey(),
+			Data:          root.GetData(),
+		}
+
+		// 递归寻找子节点
+		recursiveTree(treeNode, childs)
+
+		trees = append(trees, *treeNode)
+	}
+	return
+}
+
+func recursiveTree(node *TreeNode, childs []ITreeNode) {
+	iNode := node.Data.(ITreeNode)
+
+	for _, v := range childs {
+		if v.GetParentPrimKey() == iNode.GetPrimKey() {
+			childTreeNode := &TreeNode{
+				Name:          v.GetName(),
+				PrimKey:       v.GetPrimKey(),
+				ParentPrimKey: v.GetParentPrimKey(),
+				Data:          v.GetData(),
+			}
+
+			node.Children = append(node.Children, *childTreeNode)
+		}
+	}
+}
---
+++ library/common/tree_test.go
@@ -0,0 +1,58 @@
+package common
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gogf/gf/test/gtest"
+	"testing"
+)
+
+type Tee struct {
+	Id       int
+	Name     string
+	ParentId int
+}
+
+func (r Tee) GetPrimKey() int {
+	return r.Id
+}
+
+func (r Tee) GetParentPrimKey() int {
+	return r.ParentId
+}
+
+func (r Tee) GetName() string {
+	return r.Name
+}
+
+func (r Tee) GetData() interface{} {
+	return r
+}
+
+func (r Tee) Root() bool {
+	return r.ParentId == 0
+}
+
+func getData(datas []Tee) (iTrees []ITreeNode) {
+	for _, v := range datas {
+		iTrees = append(iTrees, v)
+	}
+	return
+}
+
+func TestCreateRelation(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		datas := []Tee{
+			{Id: 1, Name: "根节点1", ParentId: 0},
+			{Id: 2, Name: "根节点2", ParentId: 0},
+			{Id: 3, Name: "根节点3", ParentId: 0},
+			{Id: 21, Name: "子节点1", ParentId: 2},
+			{Id: 11, Name: "子节点2", ParentId: 1},
+			{Id: 31, Name: "子节点3", ParentId: 3},
+		}
+		tree := GenerateTree(getData(datas))
+		by, _ := json.Marshal(tree)
+		fmt.Print(string(by))
+		t.AssertNE(tree, nil)
+	})
+}
